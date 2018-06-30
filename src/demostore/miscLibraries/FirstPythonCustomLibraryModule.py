b3043f2a1ca5652a2d31328faddfe78712cff112
John P Quense <jpquense@gmail.com>
2018-06-30 07:52:43 -0400

    initial setup

---
+++ src/demostore/miscLibraries/FirstPythonCustomLibraryModule.py
@@ -0,0 +1,22 @@
+
+
+from robot.api import logger
+
+
+def first_method_in_custom():
+    """
+
+    :return:
+    """
+
+    logger.console("FIRST CUSTOM METHOD IN MODULE")
+
+
+def second_method_in_custom():
+    """
+
+    :return:
+    """
+
+    logger.console("FIRST CUSTOM METHOD IN MODULE")
+
